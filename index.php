d859dfeb2bbb8d3ec71b0b07ab9adcb09284a050
Hitarth Dhaduk <hitarthdhaduk@Hitarths-MacBook-Air.local>
2026-06-28 17:24:55 +0530

    cleanup: remove legacy PHP files, assets, and database.sql

--- index.php
+++
@@ -1,3 +0,0 @@
-<?php
-header('Location: /inventoryhub/login.php');
-exit;
--- logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once __DIR__ . '/includes/config.php';
-session_destroy();
-header('Location: /inventoryhub/login.php');
-exit;
